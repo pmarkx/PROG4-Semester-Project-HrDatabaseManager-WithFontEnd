7e0f782d5921e24adc9a5b61a3e87ace99241acc
Y9EEPN740 <Y9EEPN740@ISSCPF19WB0C>
2021-10-04 16:55:30 +0200

    Add project files.

---
+++ DCQEB4_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCQEB4_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DCQEB4_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DCQEB4_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
