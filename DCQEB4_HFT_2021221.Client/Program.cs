5251b54232d6699cc12f81cc22fea59218ea2d19
Papp Márk <pmarkx@stud.uni-obuda.hu>
2021-10-07 20:21:20 +0200

    testing

--- DCQEB4_HFT_2021221.Client/Program.cs
+++ DCQEB4_HFT_2021221.Client/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using DCQEB4_HFT_2021221.Data;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace DCQEB4_HFT_2021221.Client
 {
+    static class Operations
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string header)
+        {
+            Console.WriteLine($"************* {header} ************");
+            foreach (var item in input) Console.WriteLine(item);
+            Console.WriteLine($"************* {header} ************");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            HrDbContex data = new HrDbContex();
+            data.Employees.Select(x => x.Name).ToConsole("1"); 
+
         }
     }
 }
