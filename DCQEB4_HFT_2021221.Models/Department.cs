f53ff476fe3fc696b09c0b450773764f1ec0e9ff
Papp Márk <pmarkx@stud.uni-obuda.hu>
2021-10-07 15:13:07 +0200

    Models Created

---
+++ DCQEB4_HFT_2021221.Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCQEB4_HFT_2021221.Models
+{
+    class Department
+    {
+    }
+}
