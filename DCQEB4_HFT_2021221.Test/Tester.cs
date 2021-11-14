260514d9f619bb509a85651cd85710e8c7e99dfc
Papp Márk <pmarkx@stud.uni-obuda.hu>
2021-11-14 12:45:05 +0100

    Tester class created

---
+++ DCQEB4_HFT_2021221.Test/Tester.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCQEB4_HFT_2021221.Test
+{
+    [TestFixture]
+    class Tester
+    {
+    }
+}
