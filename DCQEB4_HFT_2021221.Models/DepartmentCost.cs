c17ad065963699c54e7816f5d6cfdcbb42613a5f
Papp Márk <pmarkx@stud.uni-obuda.hu>
2021-11-17 16:43:52 +0100

    for logic

---
+++ DCQEB4_HFT_2021221.Models/DepartmentCost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCQEB4_HFT_2021221.Models
+{
+    public class DepartmentCost
+    {
+        public string DepartmentName { get; set; }
+        public double AvargePrice { get; set; }
+    }
+}
