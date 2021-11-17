a751edebee6850913808c924b17ceffbc9c55b27
Papp Márk <pmarkx@stud.uni-obuda.hu>
2021-11-17 16:44:06 +0100

    Testing

--- DCQEB4_HFT_2021221.Client/Program.cs
+++ DCQEB4_HFT_2021221.Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using DCQEB4_HFT_2021221.Data;
+using DCQEB4_HFT_2021221.Logic;
+using DCQEB4_HFT_2021221.Repository;
 using System.Linq;
 using System;
 using System.Collections.Generic;
@@ -23,7 +25,15 @@ namespace DCQEB4_HFT_2021221.Client
             data.Employees.Select(x => x.Name).ToConsole("1");
             data.Departments.Select(x => x.DepartmentName).ToConsole("2");
             data.Salaries.Select(x => x.ID).ToConsole("3");
-
+            DepartmentRepository departmentRepository = new DepartmentRepository(data);
+            EmployeeRepository employeeRepository = new EmployeeRepository(data);
+            SalaryRepository salaryRepository = new SalaryRepository(data);
+            DepartmentLogic departmentLogic = new DepartmentLogic(departmentRepository,employeeRepository,salaryRepository);
+            var a=departmentLogic.DepartmentCost();
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.DepartmentName+" avarge cost is: "+item.AvargePrice+"$/mounth");
+            }
         }
     }
 }
