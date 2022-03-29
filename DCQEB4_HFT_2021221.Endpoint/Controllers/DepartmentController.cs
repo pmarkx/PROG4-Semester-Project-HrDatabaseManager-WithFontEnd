d0984931a8e4baeca3a9d6a37505108d556dbdeb
Papp Márk <pmarkx@stud.uni-obuda.hu>
2022-03-29 23:28:41 +0200

    signalR added

--- DCQEB4_HFT_2021221.Endpoint/Controllers/DepartmentController.cs
+++ DCQEB4_HFT_2021221.Endpoint/Controllers/DepartmentController.cs
@@ -6,6 +6,8 @@ using DCQEB4_HFT_2021221.Logic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using DCQEB4_HFT_2021221.Endpoint.Services;
 
 namespace DCQEB4_HFT_2021221.Endpoint.Controllers
 {
@@ -14,9 +16,11 @@ namespace DCQEB4_HFT_2021221.Endpoint.Controllers
     public class DepartmentController : ControllerBase
     {
         IDepartmentLogic dep;
-        public DepartmentController(IDepartmentLogic dep)
+        IHubContext<SignalRHub> hub;
+        public DepartmentController(IDepartmentLogic dep, IHubContext<SignalRHub> hub)
         {
             this.dep = dep;
+            this.hub = hub;
         }
         [HttpGet]
         public IEnumerable<Department> Get()
@@ -32,16 +36,20 @@ namespace DCQEB4_HFT_2021221.Endpoint.Controllers
         public void Post([FromBody] Department value)
         {
             dep.Create(value);
+            hub.Clients.All.SendAsync("DepCreated", value);
         }
         [HttpPut]
         public void Put([FromBody] Department value)
         {
             dep.Update(value);
+            hub.Clients.All.SendAsync("DepUpdated", value);
         }
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var deptodelete=dep.GetOne(id);
             dep.Delete(id);
+            hub.Clients.All.SendAsync("DepDeleted", deptodelete);
         }
     }
 }
--- DCQEB4_HFT_2021221.Endpoint/Controllers/EmployeeController.cs
+++ DCQEB4_HFT_2021221.Endpoint/Controllers/EmployeeController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using DCQEB4_HFT_2021221.Endpoint.Services;
 
 namespace DCQEB4_HFT_2021221.Endpoint.Controllers
 {
@@ -14,9 +16,11 @@ namespace DCQEB4_HFT_2021221.Endpoint.Controllers
     public class EmployeeController : ControllerBase
     {
         IEmployeeLogic emp;
-        public EmployeeController(IEmployeeLogic emp)
+        IHubContext<SignalRHub> hub;
+        public EmployeeController(IEmployeeLogic emp, IHubContext<SignalRHub> hub)
         {
             this.emp = emp;
+            this.hub = hub;
         }
         [HttpGet]
         public IEnumerable<Employee> Get()
@@ -32,16 +36,20 @@ namespace DCQEB4_HFT_2021221.Endpoint.Controllers
         public void Post([FromBody] Employee value)
         {
             emp.Create(value);
+            hub.Clients.All.SendAsync("EmpCreated",value);
         }
         [HttpPut]
         public void Put([FromBody] Employee value)
         {
             emp.Update(value);
+            hub.Clients.All.SendAsync("EmpUpdated", value);
         }
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var emptodelete = emp.GetOne(id);
             emp.Delete(id);
+            hub.Clients.All.SendAsync("EmpDeleted", emptodelete);
         }
     }
 }
