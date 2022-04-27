96b2741ceccf70fe4206cd6c4bd7071c62bb9b35
Mark Papp <Mark.Papp@ibm.com>
2022-04-27 17:12:06 +0200

    JS fronted created

---
+++ DCQEB4_HFT_2021221.JSClient/wwwroot/script.js
@@ -0,0 +1,32 @@
+﻿let departments = [];
+
+fetch('http://localhost:50620/department')
+    .then(x => x.json())
+    .then(y => {
+        departments = y;
+        console.log(departments);
+        display();
+    });
+
+
+function display() {
+    departments.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.departmentName + "</td></tr>";
+    });
+}
+
+function create() {
+    let name = document.getElementById('departmentname').value;
+    fetch('http://localhost:50620/department', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { departmentName: name }),
+    })
+        .then(response => response.json())
+        .then(data => { console.log('Success:', data); })
+        .catch((error) => { console.error('Error:', error); });
+    display();
+}
